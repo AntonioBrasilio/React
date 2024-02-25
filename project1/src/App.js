7291283a7a6d0ec8d54fbe070805ccfd99dffc73
Antonio Brasilio <antonioalves05555@gmail.com>
2024-02-25 01:59:13 -0300

    feat: Using states on application

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: "Antonio Brasilio",
+            counter: 0
+        };
+    }
+
+    handlePClick = () => {
+        console.log(this)
+        this.setState({ name: "Aysla", date: 'teste' })
+    }
+
+    handleAClick = (event) => {
+        event.preventDefault();
+        const { counter } = this.state;
+        this.setState({ counter: counter + 1 });
+    }
+
+    render() {
+        const { name, counter } = this.state;
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p onClick={this.handlePClick}>{name} e esse é o número agora {counter}</p>
+                    <a onClick={this.handleAClick}
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Link
+                    </a>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
