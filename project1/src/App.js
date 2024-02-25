a861b44d70b794559ddc72a5902f8228a58f2706
Antonio Brasilio <antonioalves05555@gmail.com>
2024-02-25 04:26:33 -0300

    fix: Working with arrays

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,42 +1,37 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Component } from "react";
 
 class App extends Component {
     state = {
-        name: "Antonio Brasilio",
-        counter: 0,
-    };
-    handlePClick = () => {
-        console.log(this);
-        this.setState({ name: "Aysla", date: "teste" });
-    };
-
-    handleAClick = (event) => {
-        event.preventDefault();
-        const { counter } = this.state;
-        this.setState({ counter: counter + 1 });
+        posts: [
+            {
+                id: 1,
+                title: "Title 1",
+                body: "Body 1",
+            },
+            {
+                id: 2,
+                title: "Title 2",
+                body: "Body 2",
+            },
+            {
+                id: 3,
+                title: "Title 3",
+                body: "Body 3",
+            },
+        ],
     };
 
     render() {
-        const { name, counter } = this.state;
+        const { posts } = this.state;
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p onClick={this.handlePClick}>
-                        {name} e esse é o número agora {counter}
-                    </p>
-                    <a
-                        onClick={this.handleAClick}
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Link
-                    </a>
-                </header>
+                {posts.map((post) => (
+                    <div key={post.id}>
+                        <h1>{post.title}</h1>
+                        <p>{post.body}</p>
+                    </div>
+                ))}
             </div>
         );
     }
