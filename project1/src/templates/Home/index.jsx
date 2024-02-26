5e17e3e821de49edc3cd43f78d27cae0464e21b1
Antonio Brasilio <antonioalves05555@gmail.com>
2024-02-26 06:12:00 -0300

    feat: Pagination

--- project1/src/templates/Home/index.jsx
+++ project1/src/templates/Home/index.jsx
@@ -4,20 +4,39 @@ import "./styles.css";
 
 import { Posts } from "../../components/Posts";
 import { loadPosts } from "../../utils/load-posts";
+import { Button } from "../../components/Button";
 
 export class Home extends Component {
     state = {
         posts: [],
+        allPosts: [],
+        page: 0,
+        postsPerPage: 2,
     };
 
     async componentDidMount() {
         await this.loadPosts();
     }
 
+    loadMorePosts = () => {
+        const { page, postsPerPage, allPosts, posts } = this.state;
+        const nextPage = page + postsPerPage;
+        const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
+
+
+        posts.push(...nextPosts);
+        this.setState({ posts, page: nextPage });
+    }
+
     loadPosts = async () => {
+        const { page, postsPerPage } = this.state;
+
         const postAndImages = await loadPosts();
 
-        this.setState({ posts: postAndImages });
+        this.setState({
+            posts: postAndImages.slice(page, postsPerPage),
+            allPosts: postAndImages,
+        });
     };
 
     render() {
@@ -25,6 +44,7 @@ export class Home extends Component {
         return (
             <section className="container">
                 <Posts posts={posts} />
+                <Button onClick={this.loadMorePosts} text="Load more posts" />
             </section>
         );
     }
