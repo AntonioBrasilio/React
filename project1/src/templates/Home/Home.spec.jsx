83c927b6d037df2ac9b97dc921cf0c0c5f1d8d22
Antonio Brasilio <antonioalves05555@gmail.com>
2024-03-04 05:19:34 -0300

    feat: Search for posts tested

--- project1/src/templates/Home/Home.spec.jsx
+++ project1/src/templates/Home/Home.spec.jsx
@@ -3,6 +3,7 @@ import {
     screen,
     waitForElementToBeRemoved,
 } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { Home } from ".";
 
 describe("Home", () => {
@@ -12,10 +13,47 @@ describe("Home", () => {
 
         await waitForElementToBeRemoved(noMorePosts);
 
+        expect.assertions(3);
+
         const search = screen.getByPlaceholderText(/type your search/i);
         expect(search).toBeInTheDocument();
 
         const images = screen.getAllByRole("img");
         expect(images).toHaveLength(10);
+
+        const button = screen.getByRole("button", { name: /load more/i });
+        expect(button).toBeInTheDocument();
+    });
+
+    it("should search for posts", async () => {
+        render(<Home />);
+        const noMorePosts = screen.getByText("No more posts");
+
+        await waitForElementToBeRemoved(noMorePosts);
+
+        expect.assertions(7);
+
+        const search = screen.getByPlaceholderText(/type your search/i);
+        expect(screen.getAllByRole("heading")).toHaveLength(10);
+        expect(
+            screen.queryByText(/et ea vero quia laudantium autem/i),
+        ).not.toBeInTheDocument();
+
+        userEvent.type(search, "et ea vero quia laudantium autem");
+        expect(screen.getAllByRole("heading")).toHaveLength(2);
+        expect(
+            screen.getByRole("heading", {
+                name: /search value: et ea vero quia laudantium autem/i,
+            }),
+        ).toBeInTheDocument();
+
+        userEvent.clear(search);
+        expect(screen.getAllByRole("heading")).toHaveLength(10);
+        expect(
+            screen.queryByText(/et ea vero quia laudantium autem/i),
+        ).not.toBeInTheDocument();
+
+        userEvent.type(search, "testtesttesttesttest");
+        expect(screen.getByText("No more posts")).toBeInTheDocument();
     });
 });
