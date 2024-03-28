fa2c8e4b4e5aa0ef35ec63d131e482941bb87ab7
Antonio Brasilio <antonioalves05555@gmail.com>
2024-03-28 12:47:34 -0300

    feat: Creating new file for useFetch()

--- project4/src/templates/App/index.jsx
+++ project4/src/templates/App/index.jsx
@@ -1,52 +1,6 @@
 import { useState } from 'react';
 import './styles.css';
-import { useEffect } from 'react';
-import { useRef } from 'react';
-
-const useFetch = (url, options) => {
-    const [shouldUpdate, setShouldUpdate] = useState(false);
-    const [result, setResult] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const refOptions = useRef(options);
-    const refUrl = useRef(url);
-
-    const isObjectEqual = (a, b) => {
-        return JSON.stringify(a) === JSON.stringify(b);
-    };
-
-    useEffect(() => {
-        let changed = false;
-        if (!isObjectEqual(url, refUrl.current)) {
-            refUrl.current = url;
-            changed = true;
-        }
-        if (!isObjectEqual(options, refOptions.current)) {
-            refOptions.current = options;
-            changed = true;
-        }
-        if (changed) {
-            setShouldUpdate((s) => !s);
-        }
-    }, [url, options]);
-
-    useEffect(() => {
-        setLoading(true);
-        const fetchData = async () => {
-            try {
-                const response = await fetch(refUrl.current, refOptions.current);
-                const responseJson = await response.json();
-                setResult(responseJson);
-                setLoading(false);
-            } catch (error) {
-                setLoading(false);
-                throw error;
-            }
-        };
-        fetchData();
-    }, [shouldUpdate]);
-
-    return [result, loading];
-};
+import { useFetch } from '../hooks/use-fetch';
 
 function App() {
     const options = {
